334f19b65bd29836f3fc1b9dbed4d8785b20f344
nyxoshin <mascreep228@gmail.com>
2023-09-16 07:33:55 +0300

    fix: delete unnecessary css files + add main.css to fix body margin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Stats, OrbitControls, Circle } from '@react-three/drei'
+import { OrbitControls } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import './styles/app.css'
@@ -16,12 +16,7 @@ export default function App() {
           position={[0, 1, 0]}
           children-0-castShadow
         />
-        {/* <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
-          <meshStandardMaterial />
-        </Circle> */}
         <OrbitControls target={[0, 1, 0]} />
-        {/* <axesHelper args={[5]} />
-        <Stats /> */}
       </Canvas>
     </div>
   )
