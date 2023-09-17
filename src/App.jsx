7f9f4506f57cef6a753d9e8a0fb6b83eaf1c6e7b
Roman S <romashushkov97@gmail.com>
2023-09-17 19:21:17 +0300

    новый вид сцены, эмбиент, тени

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { OrbitControls, PerspectiveCamera, Sky } from "@react-three/drei";
+import { PerspectiveCamera, Sky } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
+import {
+  useGLTF,
+  Stage,
+  Grid,
+  OrbitControls,
+  Environment,
+} from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import "./styles/app.css";
 import { Camera } from "three";
@@ -10,17 +17,68 @@ export default function App() {
 
   return (
     <div className="canvas--container">
-      <Canvas camera={{ position: [-0.5, 1, 2] }} className="canvas--main">
+      {/* <Canvas
+        shadows
+        dpr={[1, 1.5]}
+        camera={{ position: [-1.5, 1, 5.5], fov: 45, near: 1, far: 20 }}
+        eventSource={document.getElementById("root")}
+        eventPrefix="client"
+        className="canvas--main"
+      > */}
+      <Canvas
+        gl={{ logarithmicDepthBuffer: true }}
+        shadows
+        camera={{ position: [-15, 0, 10], fov: 25 }}
+      >
+        {/* <color attach="background" args={["#4a5861"]} /> */}
+        {/* <fog attach="fog" args={["white", 15, 50]} /> */}
+        <hemisphereLight intensity={0.15} groundColor="black" />
+        <Stage intensity={0.5} environment="city" adjustCamera={false}>
+          <primitive
+            object={gltf.scene}
+            position={[0, -1.85, 0]}
+            scale={5}
+            children-0-castShadow
+          />
+        </Stage>
+        <spotLight
+          position={[10, 20, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize={1024}
+        />
+        <OrbitControls
+          autoRotate
+          autoRotateSpeed={0.05}
+          enableZoom={false}
+          keyEvents={true}
+          makeDefault
+          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2}
+          keys={{ LEFT: "a", RIGHT: "d" }}
+        />
         <directionalLight position={[-3.3, -0.1, -4.4]} castShadow />
         <ambientLight intensity={0.6} />
-        <Sky />
-        <primitive
-          object={gltf.scene}
-          position={[0, 0.6, 0]}
-          children-0-castShadow
-        />
-        <OrbitControls target={[0, 1, 0]} />
+        {/* <PerspectiveCamera /> */}
+        {/* <Sky /> */}
+        {/* <OrbitControls keyEvents={true} camera={kets} /> */}
       </Canvas>
     </div>
   );
+}
+
+{
+  /* <Grid
+          renderOrder={-1}
+          position={[0, 0, 0]}
+          infiniteGrid
+          cellSize={0.6}
+          cellThickness={0.6}
+          sectionSize={3.3}
+          sectionThickness={1.5}
+          sectionColor={[0.5, 0.5, 10]}
+          fadeDistance={40}
+        /> */
 }
