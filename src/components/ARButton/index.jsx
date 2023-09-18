b72f57b32d69f8cef44cc35932352233b049ea85
nyxoshin <mascreep228@gmail.com>
2023-09-18 23:14:31 +0300

    feat: add style to arbutton

--- src/components/ARButton/index.jsx
+++ src/components/ARButton/index.jsx
@@ -1,13 +1,11 @@
 import React from "react";
+import './style.css'
 
 export default function ARButton() {
   return (
     <div className="container--arbutton">
-      <a rel="ar" href="./models/Apple/AR_barfits.usdz">
-        <img />
-        <button className="arbutton">
-          fjkdlsfjsd
-        </button>
+      <a rel="ar" href="./models/Apple/AR_barfits.usdz" className="link--arbutton">
+        <img src="./images/logo-ar.svg" width="50px" height="50px" />
       </a>
     </div>
   );
