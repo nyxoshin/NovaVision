772b411358b8387e077314aefe4362b03d2c7979
nyxoshin <mascreep228@gmail.com>
2023-09-19 21:51:33 +0300

    fix: fix axis center usdz

--- src/components/ARButton/index.jsx
+++ src/components/ARButton/index.jsx
@@ -4,7 +4,7 @@ import './style.css'
 export default function ARButton() {
   return (
     <div className="container--arbutton">
-      <a rel="ar" href="./models/Apple/AR_barfits.usdz" className="link--arbutton">
+      <a rel="ar" href="./models/Apple/barfits_AR.usdz" className="link--arbutton">
         <img src="./images/logo-ar.svg" width="44px" height="44px" class="arbutton--img" />
       </a>
     </div>
