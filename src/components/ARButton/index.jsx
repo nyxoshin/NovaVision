79f33828b6b78dcccdec85dd5698621b0319993c
Roman S <romashushkov97@gmail.com>
2023-10-05 21:41:05 +0300

    android button

--- src/components/ARButton/index.jsx
+++ src/components/ARButton/index.jsx
@@ -3,6 +3,10 @@ import "./style.css";
 import isMobile from "../checkDevice";
 
 export default function ARButton() {
+  var model_url = `intent://arvr.google.com/scene-viewer/1.0?file=${"./models/barfits_final.gltf"}#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;`;
+  function OpenUrl() {
+    window.open(model_url);
+  }
   return (
     <>
       {isMobile.iOS() !== null && (
@@ -17,15 +21,21 @@ export default function ARButton() {
         </div>
       )}
       {isMobile.Android() !== null && (
-        <div className="container--arbutton">
-          <a
-            rel="ar"
-            href="./models/Apple/AR_barfits.usdz"
-            className="link--arbutton"
-          >
+        <>
+          {/* <div className="container--arbutton">
+            <a
+              rel="ar"
+              href="./models/Apple/AR_barfits.usdz"
+              className="link--arbutton"
+            >
+              <img src="./images/logo-ar.svg" width="50px" height="50px" />
+            </a>
+          </div> */}
+          <div class="container--arbutton">
             <img src="./images/logo-ar.svg" width="50px" height="50px" />
-          </a>
-        </div>
+            <button onClick={() => OpenUrl()}></button>
+          </div>
+        </>
       )}
     </>
   );
