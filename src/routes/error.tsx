34e689bf0e7941e7e3cdd31a7c62b1ab24dd1953
Roman S <romashushkov97@gmail.com>
2024-03-30 22:40:51 +0300

    Revert "update"
    This reverts commit a68c148415bc2e4e6570dabe31ab7756291edc2d.

--- src/routes/error.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export function ErrorContainer(children: ReactNode) {
-  return (
-    <div className="ErrorPage">
-      <div className="main_block">{children}</div>
-    </div>
-  );
-}
