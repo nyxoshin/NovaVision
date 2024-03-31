dfcf66447d98cf98ed822f381b55b895be4f7227
Roman S <romashushkov97@gmail.com>
2024-03-31 23:24:42 +0300

    check out

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import Loader from "./components/Loader";
 import ARButton from "./components/ARButton";
 import isMobile from "./components/checkDevice";
 
-export default function App(name) {
+export default function AppOne(name) {
   console.log("namenamenamenamenamename", name);
-  const gltf = useLoader(GLTFLoader, `./models/${name.name}.gltf`);
+  const gltf = useLoader(GLTFLoader, `./models/Monitor.gltf`);
 
   const el = document.getElementById("canvasToTrack");
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "@fontsource/roboto";
 import "./styles/main.css";
 import Router from "./routes/routes";
+import AppOne from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Router />
+    <AppOne />
+    {/* <Router /> */}
     {/* <div>123</div> */}
   </React.StrictMode>
 );
